ebb520320d5a7c8cc2eb2cb8bf7d6602cb7856c2
Heiko van der Heijden <heikovanderheijden@Heikos-MacBook-Pro.local>
2017-06-01 16:33:49 +0200

    Removed old crap

--- PyMiniQbt/MiniQbt/Globals.py
+++
@@ -1,7 +0,0 @@
-import MiniQbtNative
-
-def getVersion():
-    return MiniQbtNative.get_version()
-
-def getName():
-    return MiniQbtNative.get_name()
--- PyMiniQbt/tests/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import MiniQbt
-
-
-print(testModule.testMethod(10))
