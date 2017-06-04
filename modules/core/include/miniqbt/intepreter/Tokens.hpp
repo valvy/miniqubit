d01fb0d54197a42f3ddc88425e71337dd9516cde
Your Name <you@example.com>
2017-06-04 14:12:03 +0000

    Linux python

--- modules/core/include/miniqbt/intepreter/Tokens.hpp
+++ modules/core/include/miniqbt/intepreter/Tokens.hpp
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <vector>
-
+#include <memory>
 
 namespace MiniQbt{
     namespace Core{
--- modules/python/setup.py
+++ modules/python/setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup, Extension
 module = Extension(
       'MiniQbtNative', 
+      language="c++",
       sources=['${PyMiniQbt_SOURCE_FILES}'],
-      extra_compile_args = ['-O3','-std=c++14'],
-      include_dirs = ['${PyMiniQbt_EIGEN_INCLUDE_LIB}']
-  #    libraries = ['MiniQbt']
+      extra_compile_args = ['-fPIC','-O3','-std=c++14'],
+      include_dirs = ['${PyMiniQbt_EIGEN_INCLUDE_LIB}'],
+      runtime_library_dirs=['/usr/local/lib'],
+      extra_link_args= ['-L/usr/local/lib', '-lMiniQbt']
+#      libraries = ['MiniQbt'],    
+#      ext_modules = ['MiniQbt']
+#runtime_libraries = ['MiniQbt'],
+      #library_dirs = ['/usr/local/lib']
 )
 
 
@@ -13,6 +19,7 @@ setup(
       license="MIT",
       version='${PACKAGE_VERSION}',
       ext_modules = [module],
+
       package_dir={ '': '${CMAKE_CURRENT_SOURCE_DIR}' },
       packages=['MiniQbt']
 
