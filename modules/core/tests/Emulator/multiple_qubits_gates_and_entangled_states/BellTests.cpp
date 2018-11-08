c814e7b0ef99025f1be965b670d53fd2292f6e11
Heiko van der Heijden <h.j.m.van.der.heijden@hva.nl>
2018-11-08 10:56:04 +0100

    Updated to catch2

--- modules/core/tests/Emulator/multiple_qubits_gates_and_entangled_states/BellTests.cpp
+++ modules/core/tests/Emulator/multiple_qubits_gates_and_entangled_states/BellTests.cpp
@@ -1,4 +1,4 @@
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 #include <miniqbt/emulator/MiniQbtEmulator.hpp>
 
 TEST_CASE( "Bell tests") {
--- modules/core/tests/InterpreterTests/SyntaxTests.cpp
+++ modules/core/tests/InterpreterTests/SyntaxTests.cpp
@@ -1,4 +1,4 @@
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 #include <miniqbt/interpreter/QasmAsyncInterpreter.hpp>
 #include <sstream>
 
